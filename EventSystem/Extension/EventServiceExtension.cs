6d31df7d271a79505d628e9d6de83334f64fdb08
arminjavidfar@live.com <2580761519>
2023-08-26 21:40:37 +0430

    implement EventSystem

---
+++ EventSystem/Extension/EventServiceExtension.cs
@@ -0,0 +1,95 @@
+﻿using EventSystem.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSystem.Extension
+{
+    public enum EventTypes
+    {
+        EventTypeExample,
+        OnEnemyDied,
+        OnBoosterUsed,
+        OnScreenTapped
+    }
+
+    public static class EventServiceExtension
+    {
+        public static void RegisterEvent(this IEventService eventSystem, EventTypes eventType, Action action)
+        {
+            eventSystem.RegisterEvent((int)eventType, action);
+        }
+
+        public static void RegisterEvent<T1>(this IEventService eventSystem, EventTypes eventType, Action<T1> action)
+        {
+            eventSystem.RegisterEvent((int)eventType, action);
+        }
+
+        public static void RegisterEvent<T1, T2>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2> action)
+        {
+            eventSystem.RegisterEvent((int)eventType, action);
+        }
+
+        public static void RegisterEvent<T1, T2, T3>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2, T3> action)
+        {
+            eventSystem.RegisterEvent((int)eventType, action);
+        }
+
+        public static void RegisterEvent<T1, T2, T3, T4>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2, T3, T4> action)
+        {
+            eventSystem.RegisterEvent((int)eventType, action);
+        }
+
+        public static void UnRegisterEvent(this IEventService eventSystem, EventTypes eventType, Action action)
+        {
+            eventSystem.UnRegisterEvent((int)eventType, action);
+        }
+
+        public static void UnRegisterEvent<T1>(this IEventService eventSystem, EventTypes eventType, Action<T1> action)
+        {
+            eventSystem.UnRegisterEvent<T1>((int)eventType, action);
+        }
+
+        public static void UnRegisterEvent<T1, T2>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2> action)
+        {
+            eventSystem.UnRegisterEvent((int)eventType, action);
+        }
+
+        public static void UnRegisterEvent<T1, T2, T3>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2, T3> action)
+        {
+            eventSystem.UnRegisterEvent((int)eventType, action);
+        }
+
+        public static void UnRegisterEvent<T1, T2, T3, T4>(this IEventService eventSystem, EventTypes eventType, Action<T1, T2, T3, T4> action)
+        {
+            eventSystem.UnRegisterEvent((int)eventType, action);
+        }
+
+        public static void BroadcastEvent(this IEventService eventSystem, EventTypes eventType)
+        {
+            eventSystem.BroadcastEvent((int)eventType);
+        }
+
+        public static void BroadcastEvent<T1>(this IEventService eventSystem, EventTypes eventType, T1 param1)
+        {
+            eventSystem.BroadcastEvent<T1>((int)eventType, param1);
+        }
+
+        public static void BroadcastEvent<T1, T2>(this IEventService eventSystem, EventTypes eventType, T1 param1, T2 param2)
+        {
+            eventSystem.BroadcastEvent((int)eventType, param1, param2);
+        }
+
+        public static void BroadcastEvent<T1, T2, T3>(this IEventService eventSystem, EventTypes eventType, T1 param1, T2 param2, T3 param3)
+        {
+            eventSystem.BroadcastEvent((int)eventType, param1, param2, param3);
+        }
+
+        public static void BroadcastEvent<T1, T2, T3, T4>(this IEventService eventSystem, EventTypes eventType, T1 param1, T2 param2, T3 param3, T4 param4)
+        {
+            eventSystem.BroadcastEvent((int)eventType, param1, param2, param3, param4);
+        }
+    }
+}
--- EventSystem/Program.cs
+++ EventSystem/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EventSystem.Abstraction;
+using EventSystem.Core;
+using EventSystem.Extension;
+
+namespace EventSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestEventSystem();
+        }
+
+        private static void TestEventSystem()
+        {
+            RegisterSampleEvents();
+            BroadcastSampleEvents();
+        }
+
+        #region Broadcast sample events
+        private static void BroadcastSampleEvents()
+        {
+            IEventService eventSystem = EventService.Instance;
+
+            eventSystem.BroadcastEvent(EventTypes.OnEnemyDied, "The Giant");
+            eventSystem.BroadcastEvent(EventTypes.OnBoosterUsed, "Potion");
+            eventSystem.BroadcastEvent(EventTypes.OnScreenTapped, 100, 550);
+        }
+        #endregion
+
+        #region Register sample events
+        private static void RegisterSampleEvents()
+        {
+            IEventService eventSystem = EventService.Instance;
+
+            eventSystem.RegisterEvent<string>(EventTypes.OnEnemyDied, EnemyDied);
+            eventSystem.RegisterEvent<string>(EventTypes.OnBoosterUsed, BoosterUsed);
+            eventSystem.RegisterEvent<float, float>(EventTypes.OnScreenTapped, ScreenTapped);
+        }
+
+        private static void ScreenTapped(float x, float y)
+        {
+            Console.WriteLine($"Screen tapped at position: {x}, {y}");
+        }
+
+        private static void BoosterUsed(string boosterName)
+        {
+            Console.WriteLine($"Booster used. Booster name: {boosterName}");
+        }
+
+        private static void EnemyDied(string enemyName)
+        {
+            Console.WriteLine($"Enemy died. Enemy name: {enemyName}");
+        }
+        #endregion
+    }
+}
